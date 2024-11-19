105e9ac4e907be9b111695699f40c6fd01b91b90
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-11-19 17:33:24 +0000

    Create review cycle model

--- reviews/models.py
+++ reviews/models.py
@@ -1,5 +1,17 @@
 from django.db import models
-from courses.models import Course 
+from courses.models import Course
 from django.conf import settings
 
+
 # Create your models here.
+
+
+class ReviewCycle(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
