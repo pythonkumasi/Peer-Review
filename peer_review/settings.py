136e2be57af19b1d43d975978c2052f4b42b6c11
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-11-19 15:18:17 +0000

    Switch the AUTH_USER_MODEL in settings.py

--- peer_review/settings.py
+++ peer_review/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# https://docs.djangoproject.com/en/dev/topics/auth/customizing/#substituting-a-custom-user-model
+AUTH_USER_MODEL = "accounts.CustomUser"
