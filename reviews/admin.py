dbeda48b671c33c20406981ecfa2fb012f3a5374
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-11-19 21:09:14 +0000

    Import all reviews app models

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import (
+    ReviewCycle,
+    StudentEnrollment,
+    ReviewAssignment,
+    Review,
+    LecturerGrade,
+)
 
 # Register your models here.
