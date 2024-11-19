9807b703bbf9d0de37169da2200e5505d29547b6
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-11-19 17:35:05 +0000

    Create student enrollment model

--- reviews/models.py
+++ reviews/models.py
@@ -15,3 +15,16 @@ class ReviewCycle(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class StudentEnrollment(models.Model):
+    student = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        limit_choices_to={"role": "student"},
+    )
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    enrolled_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student.username} enrolled in {self.course.name}"
