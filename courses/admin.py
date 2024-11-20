f37565539472f6a9d16cf7d45c7552755f3f1b9f
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-11-20 05:05:04 +0000

    Register Course model.

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Course
 
+
 # Register your models here.
-admin.site.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "code",
+        "created_by",
+    )
+
+
+admin.site.register(Course, CourseAdmin)
