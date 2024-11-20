1ce30c95a03a949e46a17e7df027d338988527b8
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-11-20 07:17:28 +0000

    Add comment about changing user password programmatically

--- accounts/admin.py
+++ accounts/admin.py
@@ -19,6 +19,14 @@ class CustomUserAdmin(admin.ModelAdmin):
         "is_staff",
         "password",
     ]
+    # Even though this can add a user in /admin/ you have to programmatically create a password for user.
+    # >>> from django.contrib.auth import get_user_model
+    # >>> User = get_user_model()
+    # >>> u = User.objects.get(username="john")
+    # >>> u.set_password("new password")
+    # >>> u.save()
+
+    # https://docs.djangoproject.com/en/5.1/topics/auth/default/#changing-passwords
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
