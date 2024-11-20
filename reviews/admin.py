f72eee6384d7de23b890f4bc8120e8d0dcb11d68
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-11-20 05:26:51 +0000

    Add ReviewAssignmentAdmin class

--- reviews/admin.py
+++ reviews/admin.py
@@ -13,8 +13,12 @@ class ReviewCycleAdmin(admin.ModelAdmin):
     list_display = ["course", "name", "start_date", "end_date"]
 
 
+class ReviewAssignmentAdmin(admin.ModelAdmin):
+    list_display = ["reviewer", "reviewee", "review_cycle", "assignment_date"]
+
+
 admin.site.register(Review)
-admin.site.register(ReviewAssignment)
+admin.site.register(ReviewAssignment, ReviewAssignmentAdmin)
 admin.site.register(ReviewCycle, ReviewCycleAdmin)
 admin.site.register(StudentEnrollment)
 admin.site.register(LecturerGrade)
